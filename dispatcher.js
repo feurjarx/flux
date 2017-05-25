8995bd107f6c6420b829d426a5143adb9c9d3988
Roman <feurjarx@gmail.com>
2017-05-25 23:26:31 +0600

    dispatcher as module

--- dispatcher.js
+++ dispatcher.js
@@ -1,59 +1,63 @@
-var Event = (function () {
+var Dispatcher = (function () {
+    var Event = (function () {
 
-    function Event(name) {
-        this.handlers = [];
-        this.name = name;
-    }
-
-    Event.prototype.addHandler = function (fn) {
-        this.handlers.push(fn);
-    };
-
-    Event.prototype.fire = function () {
-        var args = [].concat.apply([], arguments);
-        this.handlers.forEach(function (fn) {
-            fn.apply(null, args);
-        });
-    };
-
-    return Event;
-}());
-
-
-var Dispatcher = (function() {
-    var events = [];
-
-    function getEvent(name) {
-        return events.find(function (event) {
-            return event.name === name;
-        });
-    }
-
-    function dispatch() {
-        var args = [].concat.apply([], arguments);
-        var eventName = args[0];
-
-        var event = getEvent(eventName);
-        if (!event) {
-            event = new Event(eventName);
-            events.push(event);
+        function Event(name) {
+            this.handlers = [];
+            this.name = name;
         }
 
-        event.fire(args.splice(1));
-    }
+        Event.prototype.addHandler = function (fn) {
+            this.handlers.push(fn);
+        };
 
-    function subscribe(eventName, handler) {
-        var event = getEvent(eventName);
-        if (!event) {
-            event = new Event(eventName);
-            events.push(event);
+        Event.prototype.fire = function () {
+            var args = [].concat.apply([], arguments);
+            this.handlers.forEach(function (fn) {
+                fn.apply(null, args);
+            });
+        };
+
+        return Event;
+    }());
+
+
+    var EventAggregator = (function() {
+        var events = [];
+
+        function getEvent(name) {
+            return events.find(function (event) {
+                return event.name === name;
+            });
         }
 
-        event.addHandler(handler);
-    }
+        function dispatch() {
+            var args = [].concat.apply([], arguments);
+            var eventName = args[0];
 
-    return {
-        dispatch: dispatch,
-        subscribe: subscribe
-    };
+            var event = getEvent(eventName);
+            if (!event) {
+                event = new Event(eventName);
+                events.push(event);
+            }
+
+            event.fire(args.splice(1));
+        }
+
+        function subscribe(eventName, handler) {
+            var event = getEvent(eventName);
+            if (!event) {
+                event = new Event(eventName);
+                events.push(event);
+            }
+
+            event.addHandler(handler);
+        }
+
+        return {
+            dispatch: dispatch,
+            subscribe: subscribe
+        };
+    }());
+
+    return EventAggregator;
 }());
